f362b2c8d6e8924267e57471e201800af80f0809
notandy82 <notandystanek@gmail.com>
2022-12-13 14:12:00 +0000

    Add image post model

--- posts/models.py
+++ posts/models.py
@@ -14,7 +14,29 @@ class TextPost(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    
+
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
+
+
+class ImagePost(models.Model):
+    """
+    Image post model, related to owner and to party
+    """
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
 
     class Meta:
         ordering = ['-created_at']
