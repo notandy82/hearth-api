0d344e938c8ef533e546a5d2a1432888a264de06
notandy82 <notandystanek@gmail.com>
2022-12-13 13:48:45 +0000

    Comment out all post views, serializers, urls for troubleshooting

--- posts/views.py
+++ posts/views.py
@@ -1,67 +1,67 @@
-from .models import Post
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from .serializers import PostSerializer
-from hearth_api.permissions import IsOwnerOrReadOnly
+# from .models import Post
+# from rest_framework import status, permissions
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from .serializers import PostSerializer
+# from hearth_api.permissions import IsOwnerOrReadOnly
 
 
-class PostList(APIView):
-    """
-    List all posts
-    """
-    serializer_class = PostSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+# class PostList(APIView):
+#     """
+#     List all posts
+#     """
+#     serializer_class = PostSerializer
+#     permission_classes = [
+#         permissions.IsAuthenticatedOrReadOnly
+#     ]
 
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(
-            posts, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(
+#             posts, many=True, context={'request': request}
+#         )
+#         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = PostSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.errors, status=status.HTTP_201_CREATED)
+#     def post(self, request):
+#         serializer = PostSerializer(
+#             data=request.data, context={'request': request}
+#         )
+#         if serializer.is_valid():
+#             serializer.save(owner=request.user)
+#             return Response(serializer.errors, status=status.HTTP_201_CREATED)
 
 
-class PostDetail(APIView):
-    """
-    List individual posts
-    """
-    serializer_class = PostSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsOwnerOrReadOnly
-    ]
+# class PostDetail(APIView):
+#     """
+#     List individual posts
+#     """
+#     serializer_class = PostSerializer
+#     permission_classes = [
+#         permissions.IsAuthenticatedOrReadOnly,
+#         IsOwnerOrReadOnly
+#     ]
 
-    def get_object(self, pk):
-        try:
-            post = Post.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Post.DoesNotExist:
-            raise Http404
+#     def get_object(self, pk):
+#         try:
+#             post = Post.objects.get(pk=pk)
+#             self.check_object_permissions(self.request, post)
+#             return post
+#         except Post.DoesNotExist:
+#             raise Http404
 
-        def get(self, request, pk):
-            post = self.get_object(pk)
-            serializer = PostSerializer(
-                post, context={'request': request}
-            )
-            return Response(serializer.data)
+#         def get(self, request, pk):
+#             post = self.get_object(pk)
+#             serializer = PostSerializer(
+#                 post, context={'request': request}
+#             )
+#             return Response(serializer.data)
 
-        def put(self, request, pk):
-            post = self.get_object(pk)
-            serializer = PostSerializer(
-                post, data=request.data, context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         def put(self, request, pk):
+#             post = self.get_object(pk)
+#             serializer = PostSerializer(
+#                 post, data=request.data, context={'request': request}
+#             )
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
