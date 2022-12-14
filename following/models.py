20cbbc628f858afbb91d6fd1c9ddbca97c78b9be
notandy82 <notandystanek@gmail.com>
2022-12-14 20:11:54 +0000

    Add following model

--- following/models.py
+++ following/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from parties.models import Party
 
-# Create your models here.
+
+class Following(models.Model):
+    """
+    Model to create relationship between users and parties
+    """
+    owner = models.ForeignKey(
+        User, related_name='follower', on_delete=models.CASCADE
+    )
+    followed = models.ForeignKey(
+        Party, related_name='followed', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
