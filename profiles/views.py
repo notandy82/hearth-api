c48b2da8d8d83746c4f85ba4b7a31e0e2a8f7b61
notandy82 <notandystanek@gmail.com>
2022-12-14 14:48:24 +0000

    Change profile detail view to generics

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -19,31 +19,10 @@ class ProfileList(APIView):
         return Response(serializer.data)
 
 
-class ProfileDetail(APIView):
-    serializer_class = ProfileSerializer
+class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if you own it.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
