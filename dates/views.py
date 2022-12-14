79d4958934f77553f3f83e3e4cdf1cb3871cb75f
notandy82 <notandystanek@gmail.com>
2022-12-14 16:17:51 +0000

    Replace dates views with generics

--- dates/views.py
+++ dates/views.py
@@ -4,11 +4,11 @@ from rest_framework.views import APIView
 from .serializers import EventSerializer
 from .models import Event
 from rest_framework.response import Response
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from hearth_api.permissions import IsOwnerOrReadOnly
 
 
-class EventList(APIView):
+class EventList(generics.ListCreateAPIView):
     """
     List all events
     """
@@ -16,59 +16,16 @@ class EventList(APIView):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
+    queryset = Event.objects.all()
 
-    def get(self, request):
-        parties = Event.objects.all()
-        serializer = EventSerializer(
-            parties, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = EventSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class EventDetail(APIView):
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    List individual party
+    Retrieve, update, or delete event by ID if owner
     """
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = EventSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsOwnerOrReadOnly
-    ]
-
-    def get_object(self, pk):
-        try:
-            event = Event.objects.get(pk=pk)
-            self.check_object_permissions(self.request, event)
-            return event
-        except Event.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        event = self.get_object(pk)
-        serializer = EventSerializer(
-            event, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        event = self.get_object(pk)
-        serializer = EventSerializer(
-            event, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Event.objects.all()
